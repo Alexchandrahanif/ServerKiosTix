f8f21d8426d128e7773ac3652d8bf14fdb676162
Alex Chandra Hanif <alexchandra1998@gmail.com>
2024-01-27 00:26:01 +0700

    feat:create API CRUD Author Table

--- controller/author.js
+++ controller/author.js
@@ -4,6 +4,13 @@ class Controller {
   // GET ALL
   static async getAll(req, res, next) {
     try {
+      const dataAuthor = await Author.findAll();
+
+      res.status(200).json({
+        statusCode: 200,
+        message: "Berhasil Menampilkan Data Author",
+        dataAuthor,
+      });
     } catch (error) {
       next(error);
     }
@@ -12,6 +19,23 @@ class Controller {
   // GET ONE
   static async getOne(req, res, next) {
     try {
+      const { id } = req.params;
+
+      const dataAuthor = await Author.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!dataAuthor) {
+        throw { name: "Id Author Tidak Ditemukan" };
+      }
+
+      res.status(200).json({
+        statusCode: 200,
+        message: "Berhasil Menampilkan Data Author",
+        dataAuthor,
+      });
     } catch (error) {
       next(error);
     }
@@ -20,6 +44,21 @@ class Controller {
   // CREATE
   static async create(req, res, next) {
     try {
+      const { displayName, birthYear, placeOfBirth } = req.body;
+
+      const body = {
+        displayName,
+        birthYear,
+        placeOfBirth,
+      };
+
+      const dataAuthor = await Author.create(body);
+
+      res.status(201).json({
+        statusCode: 201,
+        message: "Berhasil Membuat Data Author Baru",
+        data: dataAuthor,
+      });
     } catch (error) {
       next(error);
     }
@@ -28,6 +67,31 @@ class Controller {
   // UPDATE
   static async update(req, res, next) {
     try {
+      const { id } = req.params;
+      const { displayName, birthYear, placeOfBirth } = req.body;
+
+      const dataAuthor = await Author.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!dataAuthor) {
+        throw { name: "Id Author Tidak Ditemukan" };
+      }
+
+      const body = {
+        displayName,
+        birthYear,
+        placeOfBirth,
+      };
+
+      await Author.update(body, { where: { id } });
+
+      res.status(200).json({
+        statusCode: 200,
+        message: "Berhasil Memperbaharui Data Author",
+      });
     } catch (error) {
       next(error);
     }
@@ -36,6 +100,24 @@ class Controller {
   // DELETE
   static async delete(req, res, next) {
     try {
+      const { id } = req.params;
+
+      const dataAuthor = await Author.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!dataAuthor) {
+        throw { name: "Id Author Tidak Ditemukan" };
+      }
+
+      await Author.destroy({ where: { id } });
+
+      res.status(200).json({
+        statusCode: 200,
+        message: "Berhasil Menghapus Data Author",
+      });
     } catch (error) {
       next(error);
     }
